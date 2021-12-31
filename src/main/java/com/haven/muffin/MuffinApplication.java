c411848ac4df2393a3b3b2556f659c5e2bac72fd
Sai SriHarsha Griddaluru <sriharsha.9554@gmail.com>
2021-12-31 11:13:07 +0530

    spring-setup

---
+++ src/main/java/com/haven/muffin/MuffinApplication.java
@@ -0,0 +1,13 @@
+package com.haven.muffin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuffinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MuffinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/haven/muffin/MuffinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haven.muffin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuffinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
